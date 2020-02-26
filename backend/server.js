7ceb87f4d7c19f26de5667f86a052c8566548e26
worms19 <anselmidamien@gmail.com>
2020-02-26 20:26:38 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.APP_SERVER_PORT || 8000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use(express.static(__dirname+'/../wildcard-front/build/'));
+//app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../wildcard-front/build/index.html'));
+});
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+).then(() => {
+    console.log('application connecté sur le port ' + port);
+    app.listen(port);
+}).catch((err) => {
+    console.log(err);
+});
